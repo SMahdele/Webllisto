b8a440c69dac180597a31150ad132a5c26543316
SMahdele <78979401+SMahdele@users.noreply.github.com>
2021-10-15 21:17:58 +0530

    Add files via upload

--- products/views.py
+++ products/views.py
@@ -10,7 +10,7 @@ from django.contrib.auth.decorators import login_required
 def get_category(request):
     categories = Category.objects.all()
     serializer = CategorySerializer(categories, many=True)
-    return Response(serializer.data, status=201)
+    return Response(serializer.data, status=200)
 
 
 @login_required(login_url='login')
@@ -24,20 +24,30 @@ def create_category(request):
         return Response(serializer.data, status=201)
     return Response(serializer.errors, status=400)
 
+@login_required(login_url='login')
+@api_view(["PUT"])
+def update_category(request,pk):
+    category = Category.objects.get(id=pk)
+    serializer = CategorySerializer(instance=category ,data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
 
 @login_required(login_url='login')
-@api_view(["POST"])
+@api_view(["DELETE"])
 def delete_category(request, pk):
     category = Category.objects.get(id=pk)
     category.delete()
-    return Response(status=201)
+    return Response(status=200)
 
 
 @api_view(["GET"])
 def get_products(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data, status=201)
+    return Response(serializer.data, status=200)
 
 
 
@@ -51,10 +61,21 @@ def add_product(request):
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
 
+@login_required(login_url='login')
+@api_view(["PUT"])
+def update_product(request,pk):
+    product = Product.objects.get(id=pk)
+    serializer = ProductSerializer(instance=product,data=request.data)
+    print(request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
 
 @login_required(login_url='login')
-@api_view(["POST"])
+@api_view(["DELETE"])
 def delete_product(request, pk):
     product = Product.objects.get(id=pk)
     product.delete()
-    return Response(status=201)
+    return Response(status=200)
