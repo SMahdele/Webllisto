fd776240b7d8221ac208f6959611ee1138d48f02
SMahdele <78979401+SMahdele@users.noreply.github.com>
2021-10-15 21:19:16 +0530

    Add files via upload

--- products/urls.py
+++ products/urls.py
@@ -4,9 +4,11 @@ from .views import *
 urlpatterns = [
     path('cat/', get_category, name='category_list'),
     path('cat/create/', create_category, name='category-create'),
-    path('cat/delete/<int:pk>:', create_category, name='category-delete'),
+    path('cat/update/<int:pk>', update_category, name='category-update'),
+    path('cat/delete/<int:pk>', delete_category, name='category-delete'),
     path('', get_products, name='product_list'),
     path('create/', add_product, name='product-add'),
-    path('delete/<int:pk>:', delete_product, name='product-delete'),
+    path('delete/<int:pk>', delete_product, name='product-delete'),
+    path('update/<int:pk>', update_product, name='product-update'),
 
 ]
